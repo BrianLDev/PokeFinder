6977cc6ecf425cd250a3e899cd0fef1c3db7e817
Brian Leip <BrianLeip@gmail.com>
2016-12-06 11:37:05 -0800

    Created Objective-C briding header file

---
+++ 136-PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  136-PokeFinder
+//
+//  Created by Brian Leip on 12/6/16.
+//  Copyright © 2016 Triskelion, LLC. All rights reserved.
+//
+
+#import "GeoFire.h"
