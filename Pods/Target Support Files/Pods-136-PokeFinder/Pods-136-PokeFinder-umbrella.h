91d3ad979409afb78ecc388658b4735f405daf00
Brian Leip <BrianLeip@gmail.com>
2016-12-06 11:07:43 -0800

    Firebase cocoapod installed

---
+++ Pods/Target Support Files/Pods-136-PokeFinder/Pods-136-PokeFinder-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_136_PokeFinderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_136_PokeFinderVersionString[];
+
